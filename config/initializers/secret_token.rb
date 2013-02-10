b2b488a4f284d69c8462a1062151c35d3cc276f4
israe eghribel <israe.eghribel@student.emi.ac.ma>
2013-02-10 22:38:31 +0000

    debut

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppStage::Application.config.secret_token = '6712b92395d999bdc9f537e7182136c871e078f968d4138ce4cba570cff542c184b2f87f096ada53e554d10b8a1e1eb17e63231abba70fd6092a063b7b220b2b'
